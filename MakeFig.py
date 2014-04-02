d3cb4811aab117f22f98110302cd2b5f9aff5b78
Joel <jshor@terajes-Laptop.(none)>
2014-04-02 12:25:22 -0400

    Theta rhythm graph now isolates mins and maxs

--- MakeFig.py
+++ MakeFig.py
@@ -5,9 +5,15 @@ Created on Mar 29, 2014
 '''
 from GenerateFigures.generateClusterGraphs import generate_cluster_graphs
 from GenerateFigures.generateSpikeRateGraphs import generate_spike_rate_graphs
+from GenerateFigures.generateRunLengthGraphs import generate_run_length_graphs
+from GenerateFigures.generateThetaRhythmGraphs import generate_theta_rhythm_graphs
+
 import logging
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     #generate_cluster_graphs()
-    generate_spike_rate_graphs()
+    #generate_spike_rate_graphs()
+    #generate_run_length_graphs()
+    generate_theta_rhythm_graphs()
+    
